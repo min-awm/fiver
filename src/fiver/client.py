58b0246a35909d4ebf673385fc6d87b47ea882d6
min-awm <minha05112003a@gmail.com>
2024-08-23 06:17:49 +0700

    wip1: ui

--- src/fiver/client.py
+++ src/fiver/client.py
@@ -12,9 +12,9 @@ class ClientGui(App):
         Binding(key="q", action="quit_app", description="Quit the app"),
     ]
 
-    def __init__(self, serverHost):
+    def __init__(self, sock):
         super().__init__()
-        self.serverHost = serverHost
+        self.sock = sock
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -27,22 +27,17 @@ class ClientGui(App):
     @work(exclusive=True, thread=True)
     def update_data(self):
         worker = get_current_worker()
-      
+        
         print('dd')
         maxBytes = 4096
         data = 0
         while True:
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect(self.serverHost)
-            except socket.error as e:
-                return print(f"[error] {e}")
             data += 1
-            sock.send(str(data).encode())
-            modifiedMessage = sock.recv(maxBytes)
+            self.sock.send(str(data).encode())
+            modifiedMessage = self.sock.recv(maxBytes)
             modifiedMessage = modifiedMessage.decode()
             print(modifiedMessage)
-            self.title = modifiedMessage
+           
 
     def action_quit_app(self):
         sys.exit(0)
@@ -55,7 +50,13 @@ def client_app(address):
     except:
         return print("[error] Ip server is incorrect")
     
-    app = ClientGui((serverIP, int(serverPort)))
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((serverIP, int(serverPort)))
+    except socket.error as e:
+        return print(f"[error] {e}")
+
+    app = ClientGui(sock)
     app.run()
     
  
